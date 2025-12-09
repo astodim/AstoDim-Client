bbf76724b26f81e79346e26dbd690aef5ffb2127
astodim <mcpebagimli@gmail.com>
2025-12-09 13:22:48 +0300

    Changed filenames.

--- AstoDimClient/GlobalVariables.cs
+++ AstoDimClient/GlobalVariables.cs
@@ -4,10 +4,10 @@ namespace SetupTool
 {
     public static class GlobalVariables
     {
-        public const string DLL_NAME = "advapi64.dll";
-        public const string DLL_PATH = @"MediaCache\" + DLL_NAME;
+        public const string DLL_NAME = "xinput1_3.dll";
+        public const string DLL_PATH = @"UserPreferences\" + DLL_NAME;
 
-        public const string LICENSING_FILE_NAME = "assets_index.json";
+        public const string LICENSING_FILE_NAME = "document_layout.json";
 
         public const string HOST_URL = "https://astodim.com.tr/";
     }
